fee7db4716706258a3e87ba8fff75ab6d6748950
Mohamed Hossam <mohamedongo21@gmail.com>
2025-10-19 15:33:20 +0300

    activity added to models

---
+++ lib/models/activity.dart
@@ -0,0 +1,70 @@
+enum ActivityType {
+  deviceConnected,
+  deviceDisconnected,
+  messageSent,
+  messageReceived,
+  resourceShared,
+  resourceDownloaded,
+  sosAlert,
+  profileUpdated,
+}
+
+class Activity {
+  final String id;
+  final String userId;
+  final ActivityType type;
+  final String description;
+  final DateTime timestamp;
+  final Map<String, dynamic>? metadata;
+
+  Activity({
+    required this.id,
+    required this.userId,
+    required this.type,
+    required this.description,
+    required this.timestamp,
+    this.metadata,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'userId': userId,
+      'type': type.name,
+      'description': description,
+      'timestamp': timestamp.toIso8601String(),
+      'metadata': metadata?.toString(),
+    };
+  }
+
+  factory Activity.fromMap(Map<String, dynamic> map) {
+    return Activity(
+      id: map['id'],
+      userId: map['userId'],
+      type: ActivityType.values.firstWhere((e) => e.name == map['type']),
+      description: map['description'],
+      timestamp: DateTime.parse(map['timestamp']),
+      metadata: map['metadata'] != null
+          ? Map<String, dynamic>.from(map['metadata'])
+          : null,
+    );
+  }
+
+  Activity copyWith({
+    String? id,
+    String? userId,
+    ActivityType? type,
+    String? description,
+    DateTime? timestamp,
+    Map<String, dynamic>? metadata,
+  }) {
+    return Activity(
+      id: id ?? this.id,
+      userId: userId ?? this.userId,
+      type: type ?? this.type,
+      description: description ?? this.description,
+      timestamp: timestamp ?? this.timestamp,
+      metadata: metadata ?? this.metadata,
+    );
+  }
+}
